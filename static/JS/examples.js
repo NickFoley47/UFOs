ff81a478803910914532276bdbc6e6f295c0ea70
Nick Foley <90662291+NickFoley47@users.noreply.github.com>
2022-06-29 20:37:47 -0500

    added code and files

--- static/JS/examples.js
+++ static/JS/examples.js
@@ -73,10 +73,10 @@ function listLoop(userList) {
 // IF Examples
 
 // if-statement syntax
-if ( condition ) { code to execute }
+// if ( condition ) { code to execute }
 
 // pseudocode practice
-if (a date is entered) {
-  Filter the default data to show only the date entered
-};
+// if (a date is entered) {
+  //Filter the default data to show only the date entered
+//};
 
